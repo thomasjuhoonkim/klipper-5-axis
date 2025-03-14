e19682b91429450311e71805e4aded5c253f1cf3
Thomas Kim <thomasjuhoonkim@gmail.com>
2025-03-14 15:54:57 -0400

    ?

--- klippy/gcode.py
+++ klippy/gcode.py
@@ -8,7 +8,7 @@ import os, re, logging, collections, shlex
 class CommandError(Exception):
     pass
 
-Coord = collections.namedtuple('Coord', ('x', 'y', 'z', 'u', 'w', 'e'))
+Coord = collections.namedtuple('Coord', ('x', 'y', 'z', 'u', 'w', 'e'), defaults = (None,None,None,None,None,None,None))
 
 class GCodeCommand:
     error = CommandError
